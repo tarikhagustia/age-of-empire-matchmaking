e223c6dedc6818912a1dd706567e1ffe8c38be12
Dewa Gede <50865141+Tri334@users.noreply.github.com>
2024-07-04 12:49:02 +0800

    add basic requirements
    - add basic back end settings
    - add path for static files/assets

--- match/settings.py
+++ match/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,7 +54,7 @@ ROOT_URLCONF = "match.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR, str(BASE_DIR) + "/match/html/"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -67,6 +67,28 @@ TEMPLATES = [
     },
 ]
 
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        "DIRS": [BASE_DIR, str(BASE_DIR) + "/iform/html/",
+                 str(BASE_DIR) + "/globalapp/html/",
+                 str(BASE_DIR) + "/iform/html/",
+                 str(BASE_DIR) + "/formModule/html/"
+                 ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 WSGI_APPLICATION = "match.wsgi.application"
 
 
@@ -74,12 +96,19 @@ WSGI_APPLICATION = "match.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        # 'OPTIONS': {
+        #    'read_default_file': str(BASE_DIR) + '/my.cnf',
+        #
+        # },
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "db_aoe",
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -116,6 +145,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'globalapp/assets'),
+]
+MEDIA_URL = "files/"
+MEDIA_FILES = BASE_DIR/'files'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
